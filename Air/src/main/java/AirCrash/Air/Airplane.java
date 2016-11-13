462bf0f1fc023e9ea415fd7a77cb82354b10fb2a
Arek <sughalith@gmail.com>
2016-11-13 15:57:28 +0100

    Added classes

---
+++ Air/src/main/java/AirCrash/Air/Airplane.java
@@ -0,0 +1,28 @@
+package AirCrash.Air;
+
+public class Airplane {
+
+	private int Id;
+	private int seats;
+	private String model;
+	
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public int getSeats() {
+		return seats;
+	}
+	public void setSeats(int seats) {
+		this.seats = seats;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	
+}
---
+++ Air/src/main/java/AirCrash/Air/Journey.java
@@ -0,0 +1,53 @@
+package AirCrash.Air;
+
+import java.util.Date;
+
+public class Journey {
+
+	private int Id;
+	private String track;
+	private Date date;
+	private int time;
+	private int tickets;
+	private int expectedDuration;
+	
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getTrack() {
+		return track;
+	}
+	public void setTrack(String track) {
+		this.track = track;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public int getTime() {
+		return time;
+	}
+	public void setTime(int time) {
+		this.time = time;
+	}
+	public int getTickets() {
+		return tickets;
+	}
+	public void setTickets(int tickets) {
+		this.tickets = tickets;
+	}
+	public int getExpectedDuration() {
+		return expectedDuration;
+	}
+	public void setExpectedDuration(int expectedDuration) {
+		this.expectedDuration = expectedDuration;
+	}
+	
+	
+	
+}
